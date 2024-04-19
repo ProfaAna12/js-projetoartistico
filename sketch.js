89026bdb1d3c6f0247359239673de6515f4f6035
ProfaAna12 <167548203+ProfaAna12@users.noreply.github.com>
2024-04-19 14:26:46 -0300

    alterando a cor do fundo

--- sketch.js
+++ sketch.js
@@ -1,17 +1,16 @@
 function setup() {
-    createCanvas(400, 400);
-    background("white");
+  createCanvas(400, 400);
+  background("black");
+}
+
+function draw() {
+  
+  stroke("white");
+  fill("pink");
+
+  //console.log(mouseIsPressed);
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
   }
-  
-  function draw() {
-    
-    stroke("black");
-    fill("pink");
-  
-    //console.log(mouseIsPressed);
-  
-    if (mouseIsPressed) {
-      rect(mouseX, mouseY, 20, 35);
-    }
-  }
-  
+}
